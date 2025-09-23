54d2117f8e86d7cba909ffc73a50f197d88d53d5
shinhan <shinhan@DESKTOP-BVI2U38>
2025-09-23 12:49:16 +0900

    last of class nice job

--- MLP_test/MLP_test.cpp
+++ MLP_test/MLP_test.cpp
@@ -12,5 +12,18 @@ int main()
 
 	MultiLayer.Create(2, HiddenNode, 1, numofHiddenLayer);
 
+	MultiLayer.m_Weight[0][0][1] = -7.3061;
+	MultiLayer.m_Weight[0][1][1] = 4.7621;
+	MultiLayer.m_Weight[0][2][1] = 4.7618;
+
+	MultiLayer.m_Weight[0][0][2] = -2.8441;
+	MultiLayer.m_Weight[0][1][2] = 6.3917;
+	MultiLayer.m_Weight[0][2][2] = 6.3917;
+
+	MultiLayer.m_Weight[1][0][1] = -4.5589;
+	MultiLayer.m_Weight[1][1][1] = -10.3788;
+	MultiLayer.m_Weight[1][2][1] = 9.7691;
+
+
 	MultiLayer.Forward();
 }
